5067c5dfee2fd3c14b035bad3e27ab1cb72258f6
harshraj1617 <rajsinghharsh1611@gmail.com>
2025-03-10 15:06:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+document.getElementById("vinay").addEventListener("mouseover",() => {
+    document.getElementById("vinay").innerHTML=`<p id="first">
+                    VINAY SAINI<br>
+                </p>
+                <p id="firstt">
+                    ~secratary
+                </p>
+            
+                <ul>
+                    <li>
+                        Contact  -  12345678
+                    </li>
+
+                    <li>
+                        Instagram - <a href="">harsh_raj1617</a> 
+                    </li>
+
+                    <li>
+                        branch - AI ML
+                    </li>
+
+                    <li>
+                        Year- 1st 
+                    </li>
+                </ul>`;
+    document.getElementById("vinay").addEventListener("mouseout",() => {
+        document.getElementById("vinay").innerHTML=`<p id="first">
+                    VINAY SAINI<br>
+                </p>
+                <p id="firstt">
+                    ~secratary
+                </p>
+                <img src="vinaysaini.png" class="image">`;
+    });
+});
+
+
+
+
+document.getElementById("lisha").addEventListener("mouseover",() => {
+    document.getElementById("lisha").innerHTML=`<p id="first">
+                    LISHA GARG<br>
+                </p>
+                <p id="firstt">
+                    ~secratary
+                </p>
+            
+                <ul>
+                    <li>
+                        Contact  -  12345678
+                    </li>
+
+                    <li>
+                        Instagram - <a href="">harsh_raj1617</a> 
+                    </li>
+
+                    <li>
+                        branch - AI ML
+                    </li>
+
+                    <li>
+                        Year- 1st 
+                    </li>
+                </ul>`;
+    document.getElementById("lisha").addEventListener("mouseout",() => {
+        document.getElementById("lisha").innerHTML=`<p id="first">
+                    LISHA GARG<br>
+                </p>
+                <p id="firstt">
+                    ~secratary
+                </p>
+                <img src="lishagarg.png" class="image">`;
+    });
+});
+
+
+
+
+document.getElementById("himanshu").addEventListener("mouseover",() => {
+    document.getElementById("himanshu").innerHTML=`<p id="first">
+                    HIMANSHU KHATRI<br>
+                </p>
+                <p id="firstt">
+                    ~secratary
+                </p>
+            
+                <ul>
+                    <li>
+                        Contact  -  12345678
+                    </li>
+
+                    <li>
+                        Instagram - <a href="">harsh_raj1617</a> 
+                    </li>
+
+                    <li>
+                        branch - AI ML
+                    </li>
+
+                    <li>
+                        Year- 1st 
+                    </li>
+                </ul>`;
+    document.getElementById("himanshu").addEventListener("mouseout",() => {
+        document.getElementById("himanshu").innerHTML=`<p id="first">
+                    HIMANSHU KHATRI<br>
+                </p>
+                <p id="firstt">
+                    ~president
+                </p>
+                <img src="himanshu.png" class="image">`;
+    });
+});
+
+
+
+document.getElementById("vaibhav").addEventListener("mouseover",() => {
+    document.getElementById("vaibhav").innerHTML=`<p id="first">
+                    VINAY SAINI<br>
+                </p>
+                <p id="firstt">
+                    ~secratary
+                </p>
+            
+                <ul>
+                    <li>
+                        Contact  -  12345678
+                    </li>
+
+                    <li>
+                        Instagram - <a href="">harsh_raj1617</a> 
+                    </li>
+
+                    <li>
+                        branch - AI ML
+                    </li>
+
+                    <li>
+                        Year- 1st 
+                    </li>
+                </ul>`;
+    document.getElementById("vaibhav").addEventListener("mouseout",() => {
+        document.getElementById("vaibhav").innerHTML=`<p id="first">
+                    VINAY SAINI<br>
+                </p>
+                <p id="firstt">
+                    ~secratary
+                </p>
+                <img src="vinaysaini.png" class="image">`;
+    });
+});
